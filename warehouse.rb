2b8ac30ad7a39fd657e76b2f2c718b4e222be8c3
sandrakoryszewska <koryszewskasandra@gmail.com>
2016-07-06 20:19:19 +0200

    -adding find

--- warehouse.rb
+++ warehouse.rb
@@ -1,17 +1,32 @@
 require_relative "./product"
 
 class Warehouse
-  attr_reader :products
+  attr_reader :products, :id, :list
 
   def initialize(products)
-    @products = products
+     @products = products
+  end
+  
+  def show_all(magazine)
+  	magazine.each do |x|
+  	  puts "#{x.id} #{x.name} #{x.price}$"
+  	end
+  	
   end
 
-  #def find(id)
-  	#arr.any?{|a| a.seat_id == "value"}
+  def find_product(list)
+  	puts "Select product number"
+	choice = gets.chomp
+	id = (choice.to_i)
+    list.each do |x|
+    	if x.id == id
+    		puts "#{x.id} #{x.name} #{x.price}$"
+    	else
+    		nil
+    	end
+    end
+  end
 
-    
-  #end
 
 end
 
@@ -21,15 +36,12 @@ product3 = Product.new({name: "Pen", price: 1.3, quantity: 100})
 product4 = Product.new({name: "Glue", price: 3.2, quantity: 3 })
 product5 = Product.new({name: "Book", price: 7.4, quantity: 15})
 
-p "yolo"
-
 products_list = [product1, product2, product3, product4, product5]
 
-
 warehouse = Warehouse.new(products_list)
-p warehouse.products
+warehouse.products #daje to samo, co products_list
 
-p "I have no idea what i'm doing"
+warehouse.show_all(products_list)
 
-p products_list.find {|a| a.product.id == "2"}
+warehouse.find_product(products_list)
 
